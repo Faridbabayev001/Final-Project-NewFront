2432c0edbd30615a7785e6b6f9db979e0c112afd
Lala.M <lalamamedova97@gmail.com>
2016-11-27 04:26:44 +0400

    error messages and file type

--- app/Http/Controllers/IstekController.php
+++ app/Http/Controllers/IstekController.php
@@ -17,7 +17,6 @@ class IstekController extends Controller
 
     public function istek_add(Request $req)
     {
-      Session::flash('istekadded' , "İstəyiniz uğurla əlavə olundu və yoxlamadan keçəndən sonra dərc olunacaq.");
      $this->validate($req, [
      'title' => 'required',
          'about' => 'required',
@@ -34,28 +33,33 @@ class IstekController extends Controller
 
    $direction='image';
    $filetype=$req->file('image')->getClientOriginalExtension();
-   $filename=time().'.'.$filetype;
-   $req->file('image')->move(public_path('image'),$filename);
-
-   $data = [
-         'type_id'=>'2',
-         'title'=>$req->title,
-         'view' => '0',
-         'about'=>$req->about,
-         'location'=>$req->location,
-         'lat'=>$req->lat,
-         'lng'=>$req->lng,
-         'name'=>$req->name,
-         'phone'=>$req->phone,
-         'email'=>$req->email,
-         'image'=>$filename,
-         'org'=>$req->org,
-         'nov'=>$req->nov,
-         'deadline'=>$req->date
-       ];
-
-    Auth::user()->elanlar()->create($data);
-    return redirect('/istek-add');
-
+   if ($filetype=='jpg' || $filetype=='jpeg' || $filetype=='png') {
+     $filename=time().'.'.$filetype;
+     $req->file('image')->move(public_path('image'),$filename);
+     Session::flash('istekadded' , "İstəyiniz uğurla əlavə olundu və yoxlamadan keçəndən sonra dərc olunacaq.");
+     $data = [
+           'type_id'=>'2',
+           'title'=>$req->title,
+           'view' => '0',
+           'about'=>$req->about,
+           'location'=>$req->location,
+           'lat'=>$req->lat,
+           'lng'=>$req->lng,
+           'name'=>$req->name,
+           'phone'=>$req->phone,
+           'email'=>$req->email,
+           'image'=>$filename,
+           'org'=>$req->org,
+           'nov'=>$req->nov,
+           'deadline'=>$req->date
+         ];
+      Auth::user()->elanlar()->create($data);
+        return redirect('/istek-add');
+   }
+    else
+     {
+       Session::flash('imageerror' , "Xahiş olunur şəkili düzgun yükləyəsiniz.");
+      return redirect('/istek-add');
     }
+  }
 }
--- resources/views/pages/destek_add.blade.php
+++ resources/views/pages/destek_add.blade.php
@@ -4,7 +4,7 @@
   <div id="breadcrumb">
   <div class="container">
      <div class="row">
-          <h1 class="text-center">Yeni Dəstək</h1>
+          <h1 class="text-left">Yeni Dəstək</h1>
     </div>
   </div>
 </div>
@@ -16,38 +16,73 @@
           <div id="map"></div>
         </div>
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-          <form  action="{{url('/destek-add')}}" method="post" enctype="multipart/form-data">
+          @if (Session::has('destekadded'))
+            <div class="alert alert-success" role="alert">{{Session::get('istekadded')}}</div>
+          @endif
+          @if (Session::has('imageerror'))
+            <div class="alert alert-danger" role="alert">{{Session::get('imageerror')}}</div>
+          @endif
+          <form action="{{url('/destek-add')}}" method="post" enctype="multipart/form-data">
             {{csrf_field()}}
             <div class="col-lg-6">
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
                 <label for="name">Başlıq</label>
-                <input type="text" name="title" class="form-control" maxlength="30">
+                <input type="text" name="title" class="form-control">
+                @if ($errors->has('title'))
+                   <span class="help-block">
+                     <strong>Boşluq buraxmayın</strong>
+                   </span>
+                @endif
               </div>
-              <div class="form-group">
-                <input type="hidden" id="lat" name="lat">
-                 <input type="hidden" id="lng" name="lng">
+              <div class="form-group{{ $errors->has('location') || $errors->has('lat') && $errors->has('lng')? ' has-error' : '' }}">
                 <label for="name">Ünvan</label>
+                   <input type="hidden" id="lat" name="lat">
+                    <input type="hidden" id="lng" name="lng">
                 <input type="text" name="location" class="form-control" id="adress" placeholder="">
+                @if ($errors->has('location'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın</strong>
+                    </span>
+                  @elseif($errors->has('lat') && $errors->has('lng'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın </strong>
+                    </span>
+                @endif
               </div>
               <div class="form-group">
                 <label for="name">Təşkilat adı</label>
                 <input type="text" name="org" class="form-control">
               </div>
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('about') ? ' has-error' : '' }}">
                 <label for="name">Açıqlama</label>
                 <textarea name="about" class="form-control" rows="6" cols="80"></textarea>
+                @if ($errors->has('about'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın</strong>
+                    </span>
+                @endif
               </div>
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
                 <label for="email">Şəkil</label>
                 <input type="file" name="image" class="form-control">
+                @if ($errors->has('image'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın</strong>
+                    </span>
+                @endif
               </div>
             </div>
             <div class="col-lg-6">
-              <div class="form-group">
-                <label for="name">Ad, Soyad</label>
+              <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="city">Ad, Soyad</label>
                 <input type="text" class="form-control" name="name" value="{{Auth::user()->name}}">
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın</strong>
+                    </span>
+                @endif
               </div>
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
                 <label for="operator">Əlaqə nömrəsi</label>
                 <div class="input-group">
                     <div class="input-group-addon">
@@ -62,22 +97,37 @@
                             </select>
                         </div>
                   <input type="text" class="form-control" name="phone" value="{{Auth::user()->phone}}" maxlength="7">
+                  @if ($errors->has('phone'))
+                      <span class="help-block">
+                        <strong>Boşluq buraxmayın</strong>
+                      </span>
+                  @endif
                 </div>
               </div>
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                 <label for="password">Email</label>
-                <input type="email" name="email" class="form-control" placeholder="Password" value="{{Auth::user()->email}}">
+                <input type="email" name="email" class="form-control" placeholder="Email" value="{{Auth::user()->email}}">
+                @if ($errors->has('email'))
+                  <span class="help-block">
+                    <strong>Boşluq buraxmayın</strong>
+                  </span>
+              @endif
               </div>
-              <div class="form-group">
+              <div class="form-group{{ $errors->has('nov') ? ' has-error' : '' }}">
                 <label for="password">Növ</label>
                 <input type="text" name="nov" class="form-control">
+                @if ($errors->has('nov'))
+                    <span class="help-block">
+                      <strong>Boşluq buraxmayın</strong>
+                    </span>
+                @endif
               </div>
               <div class="form-group">
-                <label for="date">Dəstəyin müddəti</label>
+                <label for="date">İstəyin müddəti</label>
                 <input type="date" name="date" class="form-control" id="date">
               </div>
               <div class="form-group text-center">
-                <input type="submit" class="btn" name ="send" value="GÖNDƏR">
+                <input type="submit" class="btn" value="GÖNDƏR">
               </div>
             </div>
           </form>
