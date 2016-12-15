30a939726deae2c3af45932c29f111c81734e539
Naseh <nasehbadalov@gmail.com>
2016-12-15 13:49:44 +0400

    add password reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,47 +2,53 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Şifrə bərpası</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{-- {{ session('status') }} --}}
-                            Biz sizin emailinizə şifrəni bərpa etmek üçün link göndərdik zəhmət olmasa emailinizi yoxlayasınız.
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>E-Mail Addressini yazın</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                              <button type="submit" class="btn btn-primary">
-                                  <i class="fa fa-btn fa-envelope"></i> Şifrə dəyişmə linkini göndər
-                              </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+  <div id="breadcrumb">
+  <div class="container">
+     <div class="row">
+       <div class="col-lg-12">
+         <h1>Şifrə Bərpası</h1>
+       </div>
     </div>
+  </div>
 </div>
+<section id="password-reset">
+  <div class="container">
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{-- {{ session('status') }} --}}
+                    Biz sizin emailinizə şifrəni bərpa etmek üçün link göndərdik zəhmət olmasa emailinizi yoxlayasınız.
+                </div>
+            @endif
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>E-Mail Addressini yazın</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                      <button type="submit" class="btn">
+                          <i class="fa fa-btn fa-envelope"></i> Şifrə dəyişmə linkini göndər
+                      </button>
+                    </div>
+                </div>
+            </form>
+          </div>
+      </div>
+  </div>
+<section>
 @endsection
