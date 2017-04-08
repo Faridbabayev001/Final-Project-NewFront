d4e63d02703512d52911e907784619a372721ecc
nasehbadalov <nasehbadalov@gmail.com>
2017-04-09 01:01:50 +0400

    Add new admin panel view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,91 +2,83 @@
 
 @section('title','Admin panel')
 
-  @section('content')
-    <div id="page-inner">
-              <div class="row">
-                  <div class="col-md-12">
-                      <h1 class="page-header">
-                        ALFAGEN <small>Admin Panel</small>
-                      </h1>
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="col-md-3 col-sm-12 col-xs-12">
-                      <div class="panel panel-primary text-center no-boder bg-color-red">
-                          <div class="panel-body">
-                              <i class="fa fa-yelp fa-5x"></i>
-                              <h3>{{$destek_count}}</h3>
-                          </div>
-                          <div class="panel-footer back-footer-red">
-                              Dəstək sayı
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-md-3 col-sm-12 col-xs-12">
-                      <div class="panel panel-primary text-center no-boder bg-color-brown">
-                          <div class="panel-body">
-                              <i class="fa fa-users fa-5x"></i>
-                              <h3>{{$user_count}}</h3>
-                          </div>
-                          <div class="panel-footer back-footer-brown">
-                              Istifadeciler
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-md-3 col-sm-12 col-xs-12">
-                      <div class="panel panel-primary text-center no-boder bg-color-red">
-                          <div class="panel-body">
-                              <i class="fa fa-map-marker fa-5x"></i>
-                              <h3>{{$istek_count}}</h3>
-                          </div>
-                          <div class="panel-footer back-footer-red">
-                              İstək sayı
-                          </div>
-                      </div>
-                  </div>
-              </div>
+@section('content')
+    <div class="main-content container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <div class="widget widget-tile">
+                    <div id="spark1" class="chart sparkline"></div>
+                    <div class="data-info">
+                        <div class="desc">İstifadəçi sayı</div>
+                        <div class="value"><span class="indicator indicator-equal mdi mdi-chevron-right"></span><span data-toggle="counter" data-end="{{$user_count}}" class="number">0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <div class="widget widget-tile">
+                    <div id="spark2" class="chart sparkline"></div>
+                    <div class="data-info">
+                        <div class="desc">İstək sayı</div>
+                        <div class="value"><span class="indicator indicator-positive mdi mdi-chevron-right"></span><span data-toggle="counter" data-end="{{$istek_count }}" data-suffix="" class="number">0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <div class="widget widget-tile">
+                    <div id="spark3" class="chart sparkline"></div>
+                    <div class="data-info">
+                        <div class="desc">Dəstək sayı</div>
+                        <div class="value"><span class="indicator indicator-positive mdi mdi-chevron-right"></span><span data-toggle="counter" data-end="{{$destek_count}}" class="number">0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <div class="widget widget-tile">
+                    <div id="spark4" class="chart sparkline"></div>
+                    <div class="data-info">
+                        <div class="desc">Admin sayı</div>
+                        <div class="value"><span class="indicator indicator-negative mdi mdi-chevron-right"></span><span data-toggle="counter" data-end="1" class="number">0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default panel-table">
+                        <div class="panel-heading">
+                            <div class="title">İstifadəçi siyahısı</div>
+                        </div>
+                        <div class="panel-body table-responsive">
+                            <table class="table table-striped table-borderless">
+                                <thead>
+                                <tr>
+                                    <th style="width:40%;">Avatar</th>
+                                    <th style="width:40%;">Username</th>
+                                    <th style="width:20%;">Əlaqə nömrəsi</th>
+                                    <th style="width:20%;">Email</th>
+                                </tr>
+                                </thead>
+                                <tbody class="no-border-x">
+                                @foreach (array_chunk($users->getCollection()->all(), 4) as $row)
 
-              <!-- /. ROW  -->
-
-              <div class="row">
-
-                  <div class="col-md-8 col-sm-12 col-xs-12">
-
-                      <div class="panel panel-default">
-                          <div class="panel-heading">
-                              User list
-                          </div>
-                          <div class="panel-body">
-                              <div class="table-responsive">
-                                  <table class="table table-striped table-bordered table-hover">
-                                      <thead>
-                                          <tr>
-                                              <th>User Name</th>
-                                              <th>Name&Surname</th>
-                                              <th>Phone Number</th>
-                                              <th>Email</th>
-                                          </tr>
-                                      </thead>
-                                      <tbody>
-                                        @foreach (array_chunk($users->getCollection()->all(), 4) as $row)
-
-                                        @foreach($row as $user)
-                                             <tr>
-                                                <td>{{$user->username}}</td>
-                                                <td>{{$user->name}}</td>
-                                                <td>{{$user->phone}}</td>
-                                                <td>{{$user->email}}</td>
-                                             </tr>
-                                           @endforeach
-                                        @endforeach
-                                      </tbody>
-                                  </table>
-                                  {{$users->links()}}
-                              </div>
-                          </div>
-                      </div>
-
-                  </div>
-              </div>
-  @endsection
+                                    @foreach($row as $user)
+                                        <tr>
+                                            <td class="user-avatar"> <img src="/image/{{$user->avatar}}" alt="Avatar">Penelope Thornton</td>
+                                            <td>{{$user->username}}</td>
+                                            <td>{{$user->phone}}</td>
+                                            <td>{{$user->email}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endforeach
+                                </tbody>
+                            </table>
+                            {{$users->links()}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
